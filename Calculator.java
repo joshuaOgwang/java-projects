fec5061af9f8c14e2a986a4d228db2d488541dcb
Joshua Ogwang <joshuaogwang46@gmail.com>
2020-07-02 20:30:19 +0300

    Added Calculator.java - calculator program that uses command line args

---
+++ Calculator.java
@@ -0,0 +1,33 @@
+public class Calculator {
+    public static void main(String[] args) {
+
+        // Check the number of strings passed
+        if (args.length != 3) {
+            System.out.println("Usage: java Calculator operand1 operator operand2");
+            System.exit(0);
+        }
+
+        // Variable to store the result oof the operation
+        int result = 0;
+
+        // Determine the oeraator
+        // checking second array index
+        switch (args[1].charAt(0)) {
+            case '+':
+                result = Integer.parseInt(args[0]) + Integer.parseInt(args[2]);
+                break;
+            case '-':
+                result = Integer.parseInt(args[0]) - Integer.parseInt(args[2]);
+                break;
+            case '*':
+                result = Integer.parseInt(args[0]) * Integer.parseInt(args[2]);
+                break;
+            case '/':
+                result = Integer.parseInt(args[0]) / Integer.parseInt(args[2]);
+                break;
+        }
+
+        // Display result
+        System.out.println(args[0] + " " + args[1] + " " + args[2] + " = " + result);
+    }
+}
